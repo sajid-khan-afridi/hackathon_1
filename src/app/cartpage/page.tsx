923dc4039757ebce43d5b72c57ab5ed392fc017d
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-07-02 14:28:48 +0500

    a

--- src/app/cartpage/page.tsx
+++ src/app/cartpage/page.tsx
@@ -1,5 +1,5 @@
 import Wrapper from "../../../components/Wrapper";
-import Cart from "../../../components/section/Cart";
+import Cart from "@/components/Cart";
 
 const Page = () => {
   return (
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,37 +5,76 @@ import { MdDelete } from "react-icons/md";
 
 const Cart = () => {
   return (
-    <div className="grid lgl:grid-3col sm:grid-cols-1 md:grid-cols-2 mt-40 gap-x-10 gap-y-12">
-      <div className="">
-        <Image
-          src={"/3.png"}
-          alt="ev"
-          height={300}
-          width={200}
-          className="object-cover rounded-lg"
-        />
-      </div>
-      <div className="flex flex-row sm:gap-x-30 lg:gap-x-36">
-        <div className=" flex flex-col gap-y-5">
-          <h5>Brushed Raglan Sweatshirt</h5>
-          <h1>Dress</h1>
-          <h1>Delivery Estimation</h1>
-          <h1>5 Working Days</h1>
-          <h1>$195</h1>
-        </div>
-        <div className="flex flex-col justify-between">
-          <MdDelete className="text-4xl ml-auto" />
-          <div className="flex gap-x-2 justify-center items-center text-lg">
-            <div className="h-5 w-5 rounded-full shadow-2xl text-4xl bg-gray-200 text-black p-5 flex justify-center items-center cursor-pointer">
-              -
+    <div className="grid xl:grid-3col sm:grid-cols-1 md:grid-cols-2 mt-40 gap-x-10 gap-y-12">
+      <div className="flex flex-col gap-x-10 gap-y-10">
+        {/* first */}
+        <div className="flex sm:flex-col xl:flex-row gap-x-10">
+          <div className="">
+            <Image
+              src={"/3.png"}
+              alt="ev"
+              height={300}
+              width={200}
+              className="object-cover rounded-lg"
+            />
+          </div>
+          <div className="flex flex-row sm:gap-x-30 lg:gap-x-36">
+            <div className=" flex flex-col gap-y-5">
+              <h5>Brushed Raglan Sweatshirt</h5>
+              <h1>Dress</h1>
+              <h1>Delivery Estimation</h1>
+              <h1>5 Working Days</h1>
+              <h1>$195</h1>
             </div>
-            1
-            <div className="h-5 w-5 rounded-full shadow-2xl text-4xl  bg-white text-black p-5 flex justify-center cursor-pointer items-center border border-black">
-              +
+            <div className="flex flex-col justify-between">
+              <MdDelete className="text-4xl ml-auto" />
+              <div className="flex gap-x-2 justify-center items-center text-lg">
+                <div className="h-5 w-5 rounded-full shadow-2xl text-4xl bg-gray-200 text-black p-5 flex justify-center items-center cursor-pointer">
+                  -
+                </div>
+                1
+                <div className="h-5 w-5 rounded-full shadow-2xl text-4xl  bg-white text-black p-5 flex justify-center cursor-pointer items-center border border-black">
+                  +
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* second */}
+        <div className="flex sm:flex-col lgl:flex-row gap-x-10">
+          <div className="">
+            <Image
+              src={"/3.png"}
+              alt="ev"
+              height={300}
+              width={200}
+              className="object-cover rounded-lg"
+            />
+          </div>
+          <div className="flex flex-row sm:gap-x-30 lg:gap-x-36">
+            <div className=" flex flex-col gap-y-5">
+              <h5>Brushed Raglan Sweatshirt</h5>
+              <h1>Dress</h1>
+              <h1>Delivery Estimation</h1>
+              <h1>5 Working Days</h1>
+              <h1>$195</h1>
+            </div>
+            <div className="flex flex-col justify-between">
+              <MdDelete className="text-4xl ml-auto" />
+              <div className="flex gap-x-2 justify-center items-center text-lg">
+                <div className="h-5 w-5 rounded-full shadow-2xl text-4xl bg-gray-200 text-black p-5 flex justify-center items-center cursor-pointer">
+                  -
+                </div>
+                1
+                <div className="h-5 w-5 rounded-full shadow-2xl text-4xl  bg-white text-black p-5 flex justify-center cursor-pointer items-center border border-black">
+                  +
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <div className="bg-[#FBFCFF] flex flex-col gap-y-4 p-5">
         <h5>Order Summary</h5>
         <div className="flex gap-x-28">
