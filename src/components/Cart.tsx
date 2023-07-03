6dc74a40f2a766593fe22b53bcd32278904d49a3
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-07-03 05:25:00 +0500

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -12,6 +12,7 @@ import {
   increaseItemQuantity,
   decreaseItemQuantity,
 } from "@/store/slice/cartSlice";
+import { PiBag } from "react-icons/pi";
 
 // import { urlForImage } from "../../../sanity/lib/image";
 
@@ -44,7 +45,7 @@ const Cart = () => {
   return (
     <>
       <div>Cart - {allCart.length} items</div>
-      <div className="grid xl:grid-3col sm:grid-cols-1 md:grid-cols-2 mt-40 gap-x-10 gap-y-12">
+      <div className="grid xl:grid-3col sm:grid-cols-1 md:grid-cols-2 mt-40 gap-x-10 gap-y-12 justify-center">
         <div className="flex flex-col gap-x-10 gap-y-10">
           {allCart.map((data: any) => (
             <div
@@ -94,86 +95,26 @@ const Cart = () => {
               </div>
             </div>
           ))}
-          {/* first */}
-          {/* <div className="flex sm:flex-col xl:flex-row gap-x-10">
-          <div className="">
-            <Image
-              src={"/3.png"}
-              alt="ev"
-              height={300}
-              width={200}
-              className="object-cover rounded-lg"
-            />
-          </div>
-          <div className="flex flex-row sm:gap-x-30 lg:gap-x-36">
-            <div className=" flex flex-col gap-y-5">
-              <h5>Brushed Raglan Sweatshirt</h5>
-              <h1>Dress</h1>
-              <h1>Delivery Estimation</h1>
-              <h1>5 Working Days</h1>
-              <h1>$195</h1>
-            </div>
-            <div className="flex flex-col justify-between">
-              <MdDelete className="text-4xl ml-auto" />
-              <div className="flex gap-x-2 justify-center items-center text-lg">
-                <div className="h-5 w-5 rounded-full shadow-2xl text-4xl bg-gray-200 text-black p-5 flex justify-center items-center cursor-pointer">
-                  -
-                </div>
-                1
-                <div className="h-5 w-5 rounded-full shadow-2xl text-4xl  bg-white text-black p-5 flex justify-center cursor-pointer items-center border border-black">
-                  +
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
-          {/* second */}
-          {/* <div className="flex sm:flex-col lgl:flex-row gap-x-10">
-          <div className="">
-            <Image
-              src={"/3.png"}
-              alt="ev"
-              height={300}
-              width={200}
-              className="object-cover rounded-lg"
-            />
-          </div>
-          <div className="flex flex-row sm:gap-x-30 lg:gap-x-36">
-          <div className=" flex flex-col gap-y-5">
-              <h5>Brushed Raglan Sweatshirt</h5>
-              <h1>Dress</h1>
-              <h1>Delivery Estimation</h1>
-              <h1>5 Working Days</h1>
-              <h1>$195</h1>
-            </div>
-            <div className="flex flex-col justify-between">
-            <MdDelete className="text-4xl ml-auto" />
-              <div className="flex gap-x-2 justify-center items-center text-lg">
-                <div className="h-5 w-5 rounded-full shadow-2xl text-4xl bg-gray-200 text-black p-5 flex justify-center items-center cursor-pointer">
-                  -
-                </div>
-                1
-                <div className="h-5 w-5 rounded-full shadow-2xl text-4xl  bg-white text-black p-5 flex justify-center cursor-pointer items-center border border-black">
-                  +
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
         </div>
-
-        <div className="bg-[#FBFCFF] flex flex-col gap-y-4 p-5">
-          <h5>Order Summary</h5>
-          <div className="flex gap-x-28">
-            <h1>Quantity</h1>
-            <h1> {totalQuantity}</h1>
+        {totalQuantity ? (
+          <div className="bg-[#FBFCFF] flex flex-col gap-y-4 p-5 ">
+            <h5>Order Summary</h5>
+            <div className="flex gap-x-28">
+              <h1>Quantity</h1>
+              <h1> {totalQuantity}</h1>
+            </div>
+            <div className="flex gap-x-28">
+              <h1>Sub Total</h1>
+              <h1> $ {totalPrice}</h1>
+            </div>
+            <Button />
           </div>
-          <div className="flex gap-x-28">
-            <h1>Sub Total</h1>
-            <h1> $ {totalPrice}</h1>
+        ) : (
+          <div className="flex flex-col justify-center items-center">
+            <PiBag className="text-[150px]" />
+            <h2 >Your shopping bag is empty</h2>
           </div>
-          <Button />
-        </div>
+        )}
       </div>
     </>
   );
